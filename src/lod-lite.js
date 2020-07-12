5f955d551f3236bc3f1e74269119bf890668ed3e
Roberto Enringer <r.entringer@gmail.com>
2020-07-12 16:42:26 +0200

    user minimist for cli args and improve code

--- src/lod-lite.js
+++ src/lod-lite.js
@@ -1,54 +1,61 @@
 #! /usr/bin/env node
 
-"use strict"
+'use strict'
 
-const opendata = require("lod-opendata")
-const flow = require("xml-flow")
-const { get } = require("https")
-const path = require("path")
-const tar = require("tar")
-const fs = require("fs")
-const schema = require("./schema")
-const pack = require("../package.json")
-const util = require("util")
+const opendata = require('lod-opendata')
+const flow = require('xml-flow')
+const { get } = require('https')
+const path = require('path')
+const tar = require('tar')
+const fs = require('fs')
+const pack = require('../package.json')
+const util = require('util')
+const minimist = require('minimist')
 
-const outputFolder = fs.mkdtempSync("lod-lite-output-")
 const hrstart = process.hrtime()
 const infos = { fail: [], small: [], partial: 0, audio: 0 }
 const items = {}
-let count = 0
 
-const args = (() => {
-  const defaults = {
-    partial: false,
-    max: false,
-    split: true,
+const args = minimist(process.argv.slice(2), {
+  number: ['max'],
+  string: ['output', 'schema'],
+  boolean: ['help', 'version', 'media'],
+  alias: {
+    v: 'version',
+    h: 'help'
+  },
+  default: {
+    max: 0,
+    media: true,
+    output: 'lod-lite-output',
     help: false,
     version: false,
+    schema: './schema.js'
   }
-  process.argv.slice(2).forEach((i) => {
-    let [k, v] = i.split("=")
+})
 
-    if (k in defaults)
-      defaults[k] = !isNaN(v) || k == "max" ? Math.abs(v) || false : v === undefined || v === "true"
-  })
-
-  return defaults
+const schema = (() => {
+  try {
+    return require(args.schema)
+  } catch (err) {
+    console.error('Cannot find schema file : "%s" %s', args.schema, '\n')
+    process.exit()
+  }
 })()
 
 const helper = (cmd) => {
-  let output
+  let text
 
-  if (cmd === "help")
-    output = `lod-lite <options>\n
-    \rpartial ....... Include in extract data items without all traductions (default: false)
+  if (cmd === 'help')
+    text = `lod-lite <options>\n
     \rmax=[] ........ Number of items to be extracted. e.g. max=1000 (default: all)
-    \rsplit ......... Convert audio from base64 to mp3 and save in the file (default: true)
+    \rmedia ......... Convert audio from base64 to mp3 file (default: true)
+    \rschema ........ Path to schema file
     \rhelp .......... Output usage information
     \rversion ....... Output Lod-lite version`
-  else if (cmd === "version") output = `${pack.name} : ${pack.version}`
+  else if (cmd === 'version') text = `${pack.name} : ${pack.version}`
 
-  process.stdout.write(`\n${output}\n\n`)
+  process.stdout.write(`\n${text}\n\n`)
 
   process.exit()
 }
@@ -60,7 +67,14 @@ const progress = (progress) => {
 }
 
 const end = () => {
-  writeItems(path.join(outputFolder, `${outputFolder}.json`), JSON.stringify(items, null, 2), false)
+  writeItems(path.join(args.output, `${args.output}.json`), JSON.stringify(items, null, 2))
+  writeItems(
+    path.join(args.output, `${args.output}.js`),
+    'export default ' +
+      util.inspect(items, {
+        breakLength: 'Infinity'
+      })
+  )
 
   const hrend = process.hrtime(hrstart)
   const time = new Date(hrend[0] * 1000).toISOString().substr(11, 8)
@@ -68,15 +82,15 @@ const end = () => {
   process.stdout.cursorTo(0)
   process.stdout.clearLine()
 
-  console.info("⦿ Execution time: ", time)
-  console.info("√ Items extracted : ", Object.keys(items).length)
+  console.info('⦿ Execution time: ', time)
+  console.info('√ Items extracted : ', Object.keys(items).length)
 
-  if (infos.small.length) console.info("⚠︎ Mp3 very small: ", infos.small.length, infos.small)
-  if (args.split) console.info("☊ Audio extracted: ", infos.audio)
-  if (args.partial) console.info("? Items without all keys: ", infos.partial)
-  if (infos.fail.length > 0) console.info("✕ Unable to save items: ", infos.fail.length, infos.fail)
+  if (infos.small.length) console.info('⚠︎ Mp3 very small: ', infos.small.length, infos.small)
+  if (args.media) console.info('☊ Audio extracted: ', infos.audio)
+  if (args.partial) console.info('? Items without all keys: ', infos.partial)
+  if (infos.fail.length > 0) console.info('✕ Unable to save items: ', infos.fail.length, infos.fail)
 
-  process.stdout.write("\n")
+  process.stdout.write('\n')
 
   process.exit()
 }
@@ -86,12 +100,12 @@ const mkdir = (dir) => {
     if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true })
     return dir
   } catch (err) {
-    console.error(err.message, "\n")
+    console.error(err.message, '\n')
     process.exit()
   }
 }
 
-const getFolder = (dir) => mkdir(path.join(outputFolder, dir))
+const getFolder = (dir) => mkdir(path.join(args.output, dir))
 
 const writeItems = (filename, data, id) => {
   try {
@@ -109,65 +123,63 @@ const find = (obj, tags) => {
   Object.keys(obj).some((k) =>
     tags.includes(k)
       ? (val = obj[k])
-      : obj[k] && typeof obj[k] === "object"
+      : obj[k] && typeof obj[k] === 'object'
       ? (val = find(obj[k], tags))
       : val
   )
 
-  return typeof val === "object" ? find(val, ["$text"]) : val
+  return typeof val === 'object' ? find(val, ['$text']) : val
 }
 
 const extract = (item) => {
-  const id = item["lod:meta"]["lod:id"]
-  const obj = Object.keys(schema).reduce(
-    (obj, key) => ((obj[key] = find(item, schema[key])), obj),
-    {}
-  )
-  const hasAllKeys = Object.values(obj).filter(Boolean).length === Object.keys(schema).length
+  const id = item['lod:meta']['lod:id']
 
-  if (!args.partial && !hasAllKeys) return
-  else if (!hasAllKeys) infos.partial++
+  progress(id)
 
-  if (args.split && "audio" in obj) {
-    const buff = new Buffer.from(obj.audio, "base64")
+  const obj = Object.keys(schema).reduce((obj, key) => {
+    const val = find(item, schema[key])
+    if (val) obj[key] = val
+    return obj
+  }, {})
+
+  if (args.media && 'audio' in obj) {
+    const buff = new Buffer.from(obj.audio, 'base64')
 
     if (buff.length < 1000) infos.small.push(id)
 
-    writeItems(path.join(getFolder("audio"), `${id}.mp3`), buff, id) &&
+    writeItems(path.join(getFolder('audio'), `${id}.mp3`), buff, id) &&
       delete obj.audio &&
       infos.audio++
   }
 
   const dataJson = JSON.stringify(obj, null, 2)
-  const dataJs = "export default " + util.inspect(obj, { breakLength: "Infinity" })
+  const dataJs = 'export default ' + util.inspect(obj, { breakLength: 'Infinity' })
 
-  const saveJson = writeItems(path.join(getFolder("json"), `${id}.json`), dataJson, id)
-  const saveJs = writeItems(path.join(getFolder("js"), `${id}.js`), dataJs, id)
+  const saveJson = writeItems(path.join(getFolder('json'), `${id}.json`), dataJson, id)
+  const saveJs = writeItems(path.join(getFolder('js'), `${id}.js`), dataJs, id)
 
   if (saveJson && saveJs) items[id] = obj
 
-  progress(id)
-
-  if (++count === args.max) end()
+  if (Object.keys(items).length === args.max) end()
 }
 
-const init = async () => {
-  process.on("SIGINT", end)
+const main = async () => {
+  process.on('SIGINT', end)
 
-  Array.from(["help", "version"]).forEach((cmd) => args[cmd] === true && helper(cmd))
+  Array.from(['help', 'version']).forEach((cmd) => args[cmd] === true && helper(cmd))
 
   const {
-    resources: [{ url }],
-  } = await opendata("resources/{url}")
+    resources: [{ url }]
+  } = await opendata('resources/{url}')
 
   const tarStream = tar.t({ filter: (path) => /\.xml$/.test(path) })
 
-  tarStream.on("entry", (entry) => flow(entry).on("tag:lod:item", extract).on("end", end))
+  tarStream.on('entry', (entry) => flow(entry).on('tag:lod:item', extract).on('end', end))
 
   get(url, (resp) => resp.pipe(tarStream))
 
-  console.info(`Parsing from : ${url}`, "\n")
-
+  console.info('%sParsing from : %s %s', '\n', url, '\n')
+  console.info('Use schema file : %s %s', args.schema, '\n')
 }
 
-init()
+main()
--- src/schema.js
+++ src/schema.js
@@ -1,45 +1,45 @@
 module.exports = {
   target: [
-    "lod:renvoi-adj",
-    "lod:renvoi-adv",
-    "lod:renvoi-art",
-    "lod:renvoi-conj",
-    "lod:renvoi-int",
-    "lod:renvoi-part",
-    "lod:renvoi-prep",
-    "lod:renvoi-pron",
-    "lod:renvoi-subst",
-    "lod:renvoi-vrb"
+    'lod:renvoi-adj',
+    'lod:renvoi-adv',
+    'lod:renvoi-art',
+    'lod:renvoi-conj',
+    'lod:renvoi-int',
+    'lod:renvoi-part',
+    'lod:renvoi-prep',
+    'lod:renvoi-pron',
+    'lod:renvoi-subst',
+    'lod:renvoi-vrb'
   ],
-  id: ["lod:id"],
-  lu: ["lod:item-adresse"],
+  id: ['lod:id'],
+  lu: ['lod:item-adresse'],
   de: [
-    "lod:trad-all-domin",
-    "lod:equiv-trad-all",
-    "lod:trad-all-subord",
-    "lod:eta-explicite",
-    "lod:rs-eta-presente",
+    'lod:trad-all-domin',
+    'lod:equiv-trad-all',
+    'lod:trad-all-subord',
+    'lod:eta-explicite',
+    'lod:rs-eta-presente'
   ],
   fr: [
-    "lod:trad-fr-domin",
-    "lod:equiv-trad-fr",
-    "lod:trad-fr-subord",
-    "lod:etf-explicite",
-    "lod:rs-etf-presente",
+    'lod:trad-fr-domin',
+    'lod:equiv-trad-fr',
+    'lod:trad-fr-subord',
+    'lod:etf-explicite',
+    'lod:rs-etf-presente'
   ],
   pt: [
-    "lod:trad-pt-domin",
-    "lod:equiv-trad-po",
-    "lod:trad-pt-subord",
-    "lod:etp-explicite",
-    "lod:rs-etp-presente",
+    'lod:trad-pt-domin',
+    'lod:equiv-trad-po',
+    'lod:trad-pt-subord',
+    'lod:etp-explicite',
+    'lod:rs-etp-presente'
   ],
   en: [
-    "lod:trad-en-domin",
-    "lod:equiv-trad-en",
-    "lod:trad-en-subord",
-    "lod:ete-explicite",
-    "lod:rs-ete-presente",
+    'lod:trad-en-domin',
+    'lod:equiv-trad-en',
+    'lod:trad-en-subord',
+    'lod:ete-explicite',
+    'lod:rs-ete-presente'
   ],
-  audio: ["lod:audio"],
+  audio: ['lod:audio']
 }
